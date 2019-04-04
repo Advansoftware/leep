761d55d9088457d54eadf816e4929673cfd1e17e
advansotware <brunoantunes94@hotmail.com>
2019-04-04 15:42:21 -0300

    incio da migração do projeto
    foi criado o primeiro commit para migra o projeto do para o novo projeto

---
+++ application/controllers/Home.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bruno
+ * Date: 26/10/18
+ * Time: 03:03
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+Class Home extends CI_Controller{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    public function index(){
+        $this->template->load('template/header', 'home');
+    }
+}
